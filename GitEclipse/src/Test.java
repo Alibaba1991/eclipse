9f7efbf601cde25e4ed646dad75fe1eaa58dfe35
Abacki <mr.p1991@wp.pl>
2016-11-02 15:10:02 +0100

    Revert "Elo"
    This reverts commit e4dadba24b07865d0962f1c7fc27673ede3204c7.

--- GitEclipse/src/Test.java
+++ GitEclipse/src/Test.java
@@ -5,7 +5,6 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Testujemy gita w eclipsie");
 		System.out.println("Nowy branch");
-		System.out.println("Nelo");
 	}
 
 }
